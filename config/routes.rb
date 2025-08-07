3b7b05fcbe314617a3a990f36ab79e076ff98f04
Henry García <henrygarciaospina@gmail.com>
2025-08-07 11:05:00 -0500

    Formulario new para agregar productos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index', as: :products
   get '/products/:id', to: 'products#show', as: :product 
 end
