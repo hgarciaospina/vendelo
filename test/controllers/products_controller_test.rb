1877cfdd0811b27a6a17deaeb9d5b963dc523421
Henry García <henrygarciaospina@gmail.com>
2025-08-07 11:13:57 -0500

    Prueba unitaria del formulario new de product.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,9 +14,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select '.title', 'PS4 Fat'
     assert_select '.description', 'PS4 en buen estado'
-    assert_select '.price', '150$'
+    assert_select '.price', '150$'    
+  end  
 
+  test 'render a new product form' do
+    get new_product_path
 
-    
+    assert_response :success
+    assert_select 'form'
   end  
 end  
