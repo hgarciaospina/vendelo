b7241dff1db5ff28c4f8e0d5694326de34705938
Henry García <henrygarciaospina@gmail.com>
2025-08-11 22:18:40 -0500

    Implementación de categorias.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories, except: :show
   resources :products, path: '/'
   # Silenciar solicitudes automáticas de DevTools u otros escaneos
   get '/.well-known/*path', to: proc { [204, {}, ['']] }
