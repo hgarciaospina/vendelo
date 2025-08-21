ea087631ccc55a25a1e6748fc9c8b8a826c2e5eb
Henry García <henrygarciaospina@gmail.com>
2025-08-20 21:12:35 -0500

    Test del producto más cosotoso.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,113 +1,121 @@
-require 'test_helper'
+require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test 'render a list of products' do  
+  test "render a list of products" do
     get products_path
 
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.category', 9
+    assert_select ".product", 3
+    assert_select ".category", 9
   end
 
-  test 'render a list of products filtered by category' do  
+  test "render a list of products filtered by category" do
     get products_path(category_id: categories(:computers).id)
 
     assert_response :success
-    assert_select '.product', 1
+    assert_select ".product", 1
   end
 
-  test 'render a list of products filtered by min_price and max_price' do  
-    get products_path(min_price: 160, max_price:200)
+  test "render a list of products filtered by min_price and max_price" do
+    get products_path(min_price: 160, max_price: 200)
 
     assert_response :success
-    assert_select '.product', 1
-    assert_select 'h2', 'Nintendo Switch'
+    assert_select ".product", 1
+    assert_select "h2", "Nintendo Switch"
   end
 
-  test 'search a product by query_text' do  
-    get products_path(query_text: 'Switch')
+  test "search a product by query_text" do
+    get products_path(query_text: "Switch")
 
     assert_response :success
-    assert_select '.product', 1
-    assert_select 'h2', 'Nintendo Switch'
+    assert_select ".product", 1
+    assert_select "h2", "Nintendo Switch"
   end
-  
-  test 'render a detailed product page' do
+
+  test "sort products by expensive prices first" do
+    get products_path(order_by: "expensive")
+
+    assert_response :success
+    assert_select ".product", 3
+    assert_select ".products .product:first-child h2", "Macbook Air"
+  end
+
+  test "render a detailed product page" do
     get product_path(products(:ps4))
 
     assert_response :success
-    assert_select '.title', 'PS4 Fat'
-    assert_select '.description', 'PS4 en buen estado'
-    assert_select '.price', '150$'    
-  end  
+    assert_select ".title", "PS4 Fat"
+    assert_select ".description", "PS4 en buen estado"
+    assert_select ".price", "150$"
+  end
 
-  test 'render a new product form' do
+  test "render a new product form" do
     get new_product_path
 
     assert_response :success
-    assert_select 'form'
-  end  
+    assert_select "form"
+  end
 
-  test 'allows to create a new product' do
+  test "allows to create a new product" do
     post products_path, params: {
       product: {
-        title: 'Nintendo 64',
-        description: 'Le faltan los cables',
+        title: "Nintendo 64",
+        description: "Le faltan los cables",
         price: 45,
         category_id: categories(:videogames).id
       }
-    } 
+    }
 
     assert_redirected_to products_path
-    assert_equal flash[:notice], 'Producto creado correctamente.'
-  end  
+    assert_equal flash[:notice], "Producto creado correctamente."
+  end
 
-  test 'does not allow to create a new product with empty fields' do
+  test "does not allow to create a new product with empty fields" do
     post products_path, params: {
       product: {
-        title: '',
-        description: 'Le faltan los cables',
+        title: "",
+        description: "Le faltan los cables",
         price: 45
       }
     }
 
     assert_response :unprocessable_content
-  end  
+  end
 
-  test 'render an edit product form' do
+  test "render an edit product form" do
     get edit_product_path(products(:ps4))
 
     assert_response :success
-    assert_select 'form' 
+    assert_select "form"
   end
-  
-  test 'allows update a product' do
+
+  test "allows update a product" do
     patch product_path(products(:ps4)), params: {
       product: {
         price: 165
       }
-    } 
+    }
 
     assert_redirected_to products_path
-    assert_equal flash[:notice], 'Producto actualizado correctamente.'
+    assert_equal flash[:notice], "Producto actualizado correctamente."
   end
 
-  test 'does not allow update a product with an invalid field' do
+  test "does not allow update a product with an invalid field" do
     patch product_path(products(:ps4)), params: {
       product: {
         price: nil
       }
-    } 
+    }
 
     assert_response :unprocessable_content
   end
 
-  test 'can delete products' do
-    assert_difference('Product.count', -1) do
-      delete product_path(products(:ps4))  
-    end  
+  test "can delete products" do
+    assert_difference("Product.count", -1) do
+      delete product_path(products(:ps4))
+    end
 
     assert_redirected_to products_path
-    assert_equal flash[:notice], 'Producto eliminado correctamente.'
-  end  
+    assert_equal flash[:notice], "Producto eliminado correctamente."
+  end
 end
