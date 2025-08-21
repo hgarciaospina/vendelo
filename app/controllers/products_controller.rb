4136537eb53258611cb25d8c82f8241b99108337
Henry García <henrygarciaospina@gmail.com>
2025-08-20 21:49:41 -0500

    Se refactoriza el valor de ordenamento por defecto.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
       @products = @products.search_full_text(params[:query_text])
     end
 
-    order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, "created_at DESC")
+    order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
 
     @products = @products.order(order_by).load_async
   end
